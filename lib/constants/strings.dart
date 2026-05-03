befad7af74596150d8d7758ae70d7bbf96478d29
79231720 <shkleenaa@gmail.com>
2026-05-03 10:01:09 +0300

    Create lib/constants/strings.dart file with app text strings and messages

---
+++ lib/constants/strings.dart
@@ -0,0 +1,9 @@
+// This file contains app text strings and messages
+
+class AppStrings {
+  static const String welcomeMessage = 'Welcome to Milokids English!';
+  static const String goodbyeMessage = 'Thank you for using our app!';
+  static const String loginPrompt = 'Please log in to continue.';
+  static const String signupPrompt = 'Create an account to get started.';
+  // Add more strings as needed
+}
